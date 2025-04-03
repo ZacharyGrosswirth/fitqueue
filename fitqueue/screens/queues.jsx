85643429781037f3a949a0ad36507fcf75fc15d4
ZacharyGrosswirth <zacharygrosswirth1@gmail.com>
2025-04-03 16:32:55 -0400

    queue page + component

--- fitqueue/screens/queues.jsx
+++ fitqueue/screens/queues.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import Header from "../components/header.jsx";
+import WorkoutQueue from "../components/queue_config.jsx";
+
+const queue0 = ["Alice", "Bob", "Jack"]
+const queue1 = ["Bob", "Zach"]
 
 const Queues = () => {
   return (
     <View style={styles.container}>
-      <Header gym="Southwest Recreation Center" text="Current Queues"/>
+      <Header gym="Southwest Recreation Center" text="Joined Queues"/>
+      <WorkoutQueue title="Leg Press" waitTime="20 Minutes" queue={queue0}></WorkoutQueue>
+      <WorkoutQueue title="Squat" waitTime="10 Minutes" queue={queue1}></WorkoutQueue>
     </View>
   );
 };
